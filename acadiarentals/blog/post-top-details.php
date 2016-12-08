4649bbf2f415d06ba2f8ccece971875bc3e5972b
Linda Speight <linda@reachmaine.com>
2016-12-08 11:38:29 -0500

    Remove comments & date at top of single post.

---
+++ acadiarentals/blog/post-top-details.php
@@ -0,0 +1,12 @@
+<?php /* Mods
+	7Dec16 zig - remove comments & date
+*/ ?>
+<?php global $meta_sep; ?>
+<?php /* <nav class="post-nav meta-font secondary_text">
+	<ul class="clearfix">
+		<li class="post-meta post-comments">
+			<a href="<?php comments_link(); ?>"><?php comments_number('0','1','%');?> <?php _e(' comments','be-themes'); ?></a><span class="post-meta-sep">/</span>
+		</li>
+		<li class="post-meta"><?php echo get_the_date(); ?></li>
+	</ul>
+</nav> */ ?>
