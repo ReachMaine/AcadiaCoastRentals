9df4f6911bc5a89526d98a58f638afb41c5eab78
Linda Speight <linda@reachmaine.com>
2016-12-08 13:59:10 -0500

    working

---
+++ acadiarentals/blog/post-bottom-details.php
@@ -0,0 +1,9 @@
+<?php global $meta_sep; ?>
+<?php /* mods
+8Dec16 zig - dont show categories
+*/ ?>
+<?php /* <nav class="post-nav meta-font secondary_text">
+	<ul class="clearfix">
+		<li class="post-meta post-category"><?php _e('Under :','be-themes'); ?><?php be_themes_category_list($id); ?></li>
+	</ul>
+</nav> */ ?>
