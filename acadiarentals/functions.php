45a903267665f1086f38bff0f3cd05464a2951eb
Linda Speight <linda@reachmaine.com>
2016-12-09 11:04:11 -0500

    Add metabox for the subtitle.

--- acadiarentals/functions.php
+++ acadiarentals/functions.php
@@ -4,14 +4,15 @@
 //function be_themes_child_theme_setup() {
     load_child_theme_textdomain( 'be-themes', get_stylesheet_directory() . '/languages' );
 //}
-
+require_once(get_stylesheet_directory().'/custom/branding.php');
+  require_once(get_stylesheet_directory().'/custom/property_metabox.php');
 //enqueues our external font awesome stylesheet
 function enqueue_our_required_stylesheets(){
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
 }
 add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
 
-  require_once(get_stylesheet_directory().'/custom/branding.php');
+
   function reach_widgets_init() {
     register_sidebar(
       array(
@@ -32,8 +33,8 @@ add_filter ('the_title', 'reach_subtitle_to_post');
 function reach_subtitle_to_post($title) {
   global $post;
   global $id;
-  if ($post->post_type == 'post') {
-    $subtitle = get_post_meta($id,"subtitle", true);
+  if ( ($post->post_type == 'post') && (!is_admin()) ) {
+    $subtitle = get_post_meta($id,"property_options_subtitle", true);
     if ($subtitle) {
       $title = $title.'<br><span class="acr-subtitle">'.$subtitle."</span>";
     }
